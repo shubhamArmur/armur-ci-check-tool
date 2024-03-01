266ab6f0fed524b21ea6f4db5488b367a437f3fc
shubhamArmur <shubham@myrl.tech>
2024-03-01 18:13:52 +0530

    Add RuboCop integration and handle requested actions

--- server.rb
+++ server.rb
@@ -1,3 +1,4 @@
+require 'git'           # Use the Git Ruby library to clone a repository
 require 'sinatra/base'  # Use the Sinatra web framework
 require 'octokit'       # Use the Octokit Ruby library to interact with GitHub's REST API
 require 'dotenv/load'   # Manages environment variables
@@ -69,6 +70,8 @@ class GHAapp < Sinatra::Application
         when 'rerequested'
           create_check_run
           #ADD REQUESTED_ACTION METHOD HERE
+        when 'requested_action'
+          take_requested_action
         end
       end
     end
@@ -109,20 +112,152 @@ class GHAapp < Sinatra::Application
       )
 
       # ***** RUN A CI TEST *****
-      # MARK the check run as completed!
-      @installation_client.update_check_run(
-        @payload['repository']['full_name'],
-        @payload['check_run']['id'],
-        status: 'completed',
-        conclusion: 'success',
-        accept: 'application/vnd.github+json'
-      )
+      full_repo_name = @payload['repository']['full_name']
+      repository = @payload['repository']['name']
+      head_sha = @payload['check_run']['head_sha']
+
+      clone_repository(full_repo_name, repository, head_sha)
+
+      # ADD CODE HERE TO RUN RUBOCOP #
+
+      #RUN RuboCop on all files in the repository
+      @report = `rubocop '#{repository}' --format json`
+      logger.debug @report
+      `rm -rf #{repository}`
+      @output = JSON.parse @report
+
+      # ADD ANNOTATIONS CODE HERE #
+      annotations = []
+      # You can create a maximum of 50 annotations per request to the checks API.
+      # To add more than 50 annotations, use the "Update a check run" API endpoint.
+      # This example code limits the number of annotations to 50
+      # See /rest/reference/checks#update-a-check-run
+      # for details.
+      max_annotations = 50
+
+      # RuboCop reports the number of errors found in "offense_count"
+      if @output['summary']['offense_count']  == 0
+        conclusion = 'success'
+      else
+        conclusion = 'neutral'
+        @output['files'].each do |file|
+
+          # Only parse offenses for files in this app's repository
+          file_path = file['path'].gsub(/#{repository}\//, '')
+          annotation_level = 'notice'
+
+          # Parse each offense to get details and location
+          file['offenses'].each do |offense|
+            # Limit the number of annotations to 50
+            next if max_annotations == 0
+            max_annotations -= 1
+
+            start_line = offense['location']['start_line']
+            end_line = offense['location']['last_line']
+            start_column = offense['location']['start_column']
+            end_column = offense['location']['last_column']
+            message = offense['message']
+
+            # Create a new annotation for each error
+            annotation = {
+              path: file_path,
+              start_line: start_line,
+              end_line: end_line,
+              start_column: start_column,
+              end_column: end_column,
+              annotation_level: annotation_level,
+              message: message
+            }
+
+            # Annotations only support start and end columns on the same line
+            if start_line == end_line
+              annotation.merge({start_column: start_column, end_column: end_column})
+            end
+
+            annotations.push(annotation)
+          end
+        end
+      end
+
+      # ADD CODE HERE TO UPDATE CHECK RUN SUMARRY #
+      # Update check run summary and text parameter
+      summary = "Octo RuboCop summary \n-Offense count: #{@output['summary']['offense_count']} \n-File count: #{@output['summary']['target_file_count']}\n-Target file count: #{@output['summary']['inspected_file_count']}"
+
+      text = "Octo RuboCop version: #{@output['metadata']['rubocop_version']}"
+
+
+
+
+      # Mark the check run as complete! And if there are warnings, share them.
+        @installation_client.update_check_run(
+          @payload['repository']['full_name'],
+          @payload['check_run']['id'],
+          status: 'completed',
+          conclusion: conclusion,
+          output: {
+            title: 'Octo RuboCop',
+            summary: summary,
+            text: text,
+            annotations: annotations
+          },
+          actions: [{
+            label: 'Fix this',
+            description: 'Automatically fix all linter notices.',
+            identifier: 'fix_rubocop_notices'
+          }],
+          accept: 'application/vnd.github+json'
+        )
 
     end
 
     # ADD CLONE_REPOSITORY HELPER METHOD HERE #
+    # Clones the repository to the current working directory,
+    # updates the contents using Git pull, and checks out the ref
+    #
+    # full_repo_name - The owner and repo. Ex: octocat/hello-world
+    # repository     - The repository name
+    # ref            - The branch or commit SHA or tag to check out
+    def clone_repository(full_repo_name, repository, ref)
+      @git = Git.clone("https://x-access-token:#{@installation_token.to_s}@github.com/#{full_repo_name}.git", repository)
+      pwd = Dir.getwd()
+      Dir.chdir(repository)
+      @git
+      @git.checkout(ref)
+      Dir.chdir(pwd)
+    end
 
     # ADD TAKE_REQUESTED_ACTION HELPER METHOD HERE #
+    # Handles the check run `requested_action` event
+    # See /webhooks/event-payloads#check_run
+    def take_requested_action
+      full_repo_name = @payload['repository']['full_name']
+      repository = @payload['repository']['name']
+      head_branch = @payload['check_run']['check_suite']['head_branch']
+
+      if(@payload['requested_action']['identifier'] == 'fix_rubocop_notices')
+        clone_repository(full_repo_name, repository, head_branch)
+
+        # Sets your commit username and email address
+        @git.config('user.name', ENV['GITHUB_APP_USER_NAME'])
+        @git.config('user.email', ENV['GITHUB_APP_USER_EMAIL'])
+
+        # Automatically corrent RuboCop style errors
+        @report = `rubocop '#{repository}/*' --format json --auto-correct`
+
+        pwd = Dir.getwd()
+        Dir.chdir(repository)
+        begin
+          @git.commit_all("Automatically fix Octo RuboCop notices.")
+          @git.push("https://x-access-token:#{@installation_token.to_s}@github.com/#{full_repo_name}.git", head_branch)
+        rescue
+          # Nothing to commit!
+          puts "Nothing to commit!"
+        end
+        Dir.chdir(pwd)
+        `rm -rf '#{repository}'`
+      end
+    end
+
 
     # Saves the raw payload and converts the payload to JSON format
     def get_payload_request(request)
